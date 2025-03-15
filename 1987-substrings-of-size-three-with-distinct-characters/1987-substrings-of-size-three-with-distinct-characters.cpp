ac578cf01a8fabfbaacb4a6ebb64d4e838c3ff23
munagalachandu <158558973+munagalachandu@users.noreply.github.com>
2025-03-15 13:16:42 +0530

    Time: 3 ms (28.61%), Space: 9.5 MB (15.4%) - LeetHub

--- 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.cpp
+++ 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.cpp
@@ -3,10 +3,10 @@ public:
     int countGoodSubstrings(string s) {
        if(s.size()<3)
        return 0;
-      
+       unordered_set<char> mp;
       int ct = 0;
     for (int i = 0; i <= s.size() - 3; i++) {
-        unordered_set<char> mp;
+       mp.clear();
         mp.insert(s[i]);
         mp.insert(s[i+1]);
         mp.insert(s[i+2]);
