fbb77d8bf64ed1e55b0d89c02cc7bb4e4d7d2cc9
Munagala Chandu <158558973+munagalachandu@users.noreply.github.com>
2025-11-21 23:21:33 +0530

    Time: 51 ms (55.81%), Space: 90.9 MB (44.74%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set <int>freq;
+        for( int i:nums)
+        { if(freq.count(i)) return true;
+        freq.insert(i);}
+        return false;
+        
+    }
+};
