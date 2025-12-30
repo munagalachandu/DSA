211721f65e81b14e1131dfac326419e5c109e58c
Munagala Chandu <158558973+munagalachandu@users.noreply.github.com>
2025-12-30 20:51:25 +0530

    Time: 0 ms (100%), Space: 28.2 MB (62.08%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-
-        int n=nums.size();
-        
-        if (n==1)
-        return nums[0];
-        int max=-1;
-        int elem=0;
-int c=1;
-        sort(nums.begin(),nums.end());
-        for ( int i=0;i<n-1;i++)
-        { if( nums[i]==nums[i+1])
-        { c++;}
-        else
-        { c=1;}
-
-        if (c>max)
-        { max=c;
-        elem= nums[i];}
+        int el=nums[0];
+        int ct=1;
+        for( int i=1;i<nums.size();i++)
+        { if(ct==0)
+          { el=nums[i];
+          ct=1;
+          }
+            else if(nums[i]==el)
+            ct++;
+            else
+            { ct--;}
         }
-        return elem;
+return el;
     }
 };
-// when we sort we get 66677 then if 6==6 then count 1
