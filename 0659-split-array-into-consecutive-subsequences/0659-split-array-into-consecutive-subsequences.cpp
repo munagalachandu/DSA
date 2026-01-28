43ada74f3b46b4730a37bdd20031697375b753f5
Munagala Chandu <158558973+munagalachandu@users.noreply.github.com>
2026-01-28 22:31:44 +0530

    Time: 12 ms (80.04%), Space: 62.3 MB (30.97%) - LeetHub

---
+++ 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& nums) {
+        unordered_map<int,int> freq, need;
+
+        for (int x : nums) freq[x]++;
+
+        for (int x : nums) {
+            if (freq[x] == 0) continue;
+
+            freq[x]--;
+
+            if (need[x] > 0) {
+                need[x]--;
+                need[x+1]++;
+            }
+            else if (freq[x+1] > 0 && freq[x+2] > 0) {
+                freq[x+1]--;
+                freq[x+2]--;
+                need[x+3]++;
+            }
+            else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
